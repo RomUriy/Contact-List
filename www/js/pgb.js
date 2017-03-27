eb6da4153988f9dd056116efda3debd9b46b174a
Roman Yurkevych <romuriy@gmail.com>
2017-03-27 15:59:59 +0200

    new function

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -20,22 +20,27 @@ function BusinessCard() {
 }
 
 
-
-var myContact = navigator.contacts.create({"displayName": "Test User"});
-
-
 function onSuccess(contacts) {
-    alert('Found ' + contacts.length + ' contacts.');
+    for (var i = 0; i < contacts.length; i++) {
+        for (var j = 0; j < contacts[i].addresses.length; j++) {
+            alert("Pref: "         + contacts[i].addresses[j].pref          + "\n" +
+                "Type: "           + contacts[i].addresses[j].type          + "\n" +
+                "Formatted: "      + contacts[i].addresses[j].formatted     + "\n" +
+                "Street Address: " + contacts[i].addresses[j].streetAddress + "\n" +
+                "Locality: "       + contacts[i].addresses[j].locality      + "\n" +
+                "Region: "         + contacts[i].addresses[j].region        + "\n" +
+                "Postal Code: "    + contacts[i].addresses[j].postalCode    + "\n" +
+                "Country: "        + contacts[i].addresses[j].country);
+        }
+    }
 };
  
 function onError(contactError) {
     alert('onError!');
 };
  
-// find all contacts with 'Bob' in any name field 
-var options      = new ContactFindOptions();
-options.filter   = "Влад";
+// find all contacts 
+var options = new ContactFindOptions();
+options.filter = "";
 options.multiple = true;
-options.desiredFields = [navigator.contacts.fieldType.id];
-options.hasPhoneNumber = true;
-var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+var filter = ["displayName", "addresses"];
