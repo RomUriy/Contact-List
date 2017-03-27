98916e5fdfa548e430f731c845f809c729e9439c
Roman Yurkevych <romuriy@gmail.com>
2017-03-27 15:54:13 +0200

    Add function find contact

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -18,3 +18,24 @@ function BusinessCard() {
 	navigator.notification.alert(info);
 	
 }
+
+
+
+var myContact = navigator.contacts.create({"displayName": "Test User"});
+
+
+function onSuccess(contacts) {
+    alert('Found ' + contacts.length + ' contacts.');
+};
+ 
+function onError(contactError) {
+    alert('onError!');
+};
+ 
+// find all contacts with 'Bob' in any name field 
+var options      = new ContactFindOptions();
+options.filter   = "Влад";
+options.multiple = true;
+options.desiredFields = [navigator.contacts.fieldType.id];
+options.hasPhoneNumber = true;
+var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
